4b27ce19852856fbd6a137790871c756ab518c45
안채완 <wani9909@naver.com>
2021-09-15 15:28:30 +0900

    [수정] loading 페이지 3초후 전환, 앱 이름 수정

--- app/src/main/java/com/example/ssu_everything_contest/NorthLoading.java
+++ app/src/main/java/com/example/ssu_everything_contest/NorthLoading.java
@@ -2,6 +2,7 @@ package com.example.ssu_everything_contest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,15 +14,16 @@ public class NorthLoading extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.north_loading);
 
-        //북쪽 메인으로 가는 버튼
-        Button goNorth=(Button)findViewById(R.id.goNorthMain);
-        goNorth.setOnClickListener(new View.OnClickListener() {
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent intent =new Intent(getApplicationContext(),NorthMain.class);
+            public void run() {
+                Intent intent = new Intent(NorthLoading.this, NorthMain.class);
                 startActivity(intent);
+                finish();
             }
-        });
+        }    ,3000);
+
 
 
     }
--- app/src/main/java/com/example/ssu_everything_contest/SouthLoading.java
+++ app/src/main/java/com/example/ssu_everything_contest/SouthLoading.java
@@ -2,6 +2,7 @@ package com.example.ssu_everything_contest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,16 +14,15 @@ public class SouthLoading extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.south_loading);
 
-        //남쪽 메인으로 가는 버튼
-        Button goNorth=(Button)findViewById(R.id.goSouthMain);
-        goNorth.setOnClickListener(new View.OnClickListener() {
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent intent =new Intent(getApplicationContext(),SouthMain.class);
+            public void run() {
+                Intent intent = new Intent(SouthLoading.this, SouthMain.class);
                 startActivity(intent);
+                finish();
             }
-        });
-
+        }    ,3000);
 
     }
 }
