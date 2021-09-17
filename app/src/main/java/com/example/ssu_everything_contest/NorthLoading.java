10f25632cdfdc0f98fc45391190e5c3016c0aa45
somsom13 <somin1113@naver.com>
2021-09-18 02:17:30 +0900

    [수정] 로딩페이지, 퀴즈페이지 디자인

--- app/src/main/java/com/example/ssu_everything_contest/NorthLoading.java
+++ app/src/main/java/com/example/ssu_everything_contest/NorthLoading.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 public class NorthLoading extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.north_loading);
+
+        ImageView imageView = findViewById(R.id.iv_logo);
+        Glide.with(this).load(R.raw.loading).into(imageView);
 
         Handler handler=new Handler();
         handler.postDelayed(new Runnable() {
--- app/src/main/java/com/example/ssu_everything_contest/SouthLoading.java
+++ app/src/main/java/com/example/ssu_everything_contest/SouthLoading.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 public class SouthLoading extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.south_loading);
+
+        ImageView imageView = findViewById(R.id.iv_logo2);
+        Glide.with(this).load(R.raw.loading).into(imageView);
 
         Handler handler=new Handler();
         handler.postDelayed(new Runnable() {
